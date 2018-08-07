a10b15fe200b858b2e6b8155e8be72bc75051a65
Sean Ericksen <sericks6@uoregon.edu>
2018-08-07 14:45:30 -0700

    initial commit

---
+++ client/javascripts/gallery.js
@@ -0,0 +1,43 @@
+// jshint esversion: 6
+
+let clickHandler = function() {
+  // build AJAX request URL
+  let prefixURL = "http://api.flickr.com/services/feeds/photos_public.gne?tags=";
+  let suffixURL = "&format=json&jsoncallback=?";
+  //get value entered by user from textbox
+  let flickrTag = $("input").val();
+  //let flickrTag = "buddha";
+  let requestURL = prefixURL + flickrTag + suffixURL;
+  console.log(requestURL);
+  //clear old photos
+  $(".photos").html("");
+
+  // sending AJAX request && process response
+  $.getJSON(requestURL, function(flickrResponse) {
+    flickrResponse.items.forEach(function(item, index) {
+
+      //Flickr returns 20 images by default
+      //We need only six images for the Gallery
+      if (index < 6) {
+        // create a new JQuery element to hold the image
+        // but hide it so we can fade it in
+        let $img = $("<img>").hide();
+
+        // set the attribute to the url
+        // contained in the response
+        $img.attr("src", item.media.m);
+        $img.attr("width", "100");
+
+        // attach the img tag to the main
+        // photos element and then fade it in
+        $(".photos").append($img);
+        $img.fadeIn();
+      }
+    });
+
+  });
+};
+
+$(document).ready(() => {
+  $("button").on("click", clickHandler);
+});
